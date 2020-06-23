47475a8c7134f794bd898a2d226b6cd145b174c8
CePur <36715887+CePur@users.noreply.github.com>
2020-06-24 01:05:58 +0300

    Add QRCode Support to 2FA

---
+++ CalisiyorMu/Migrations/Auth/20200623210205_AddAppUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CalisiyorMu.Migrations.Auth
+{
+    public partial class AddAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "AspNetUsers",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "AspNetUsers");
+        }
+    }
+}
